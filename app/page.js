5cb9e7d0b5aac8c840e5e386b0d457debc3625be
Eric Yang <ericy@Erics-Air.localdomain>
2024-04-06 00:59:55 -0700

    basic setup of flex boxes and skeleton

--- app/page.js
+++ app/page.js
@@ -3,8 +3,29 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Hex
+      <div className="flex flex-col bg-black">
+        <div className="flex flex-col justify-center">
+          <img src="/innod_logo.png" width={500} height={500}></img>
+          <div className="flex flex-col self-center">
+            <div>Hex</div>
+            <div>Hone & Explore</div>
+            <div>April 20-21</div>
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <img src="/angel.png" width={300} height={300}></img>
+          <div className="flex flex-col">
+            <div>About</div>
+            <div>Placeholder Text</div>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div>Schedule</div>
+          <div className="flex flex-row">
+            <div>Sat. April 20</div>
+            <div>Sun. April 21</div>
+          </div>
+        </div>
       </div>
     </main>
   );
