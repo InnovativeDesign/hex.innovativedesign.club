9781c05f577c1d4c519c560b68332f5ad9ad32b1
Eric Yang <ey3@berkeley.edu>
2024-04-10 10:30:20 -0700

    merged schedule images into one flex box and added image spacings

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
         </div>
           
         <div className="flex flex-row items-center gap-24 ml-32 mr-32 min-w-screen bg-black">
-          <img src="/angel.png" width={360} height={360}></img>
+          <img hidden className="lg:inline-block" src="/angel.png" width={360} height={360}></img>
           <div className="flex flex-col">
             <div className="flex flex-row justify-between items-center">
               <p class="text-6xl font-bold font-druk inline-block bg-gradient-to-r from-red-500 via-yellow-500 to-green-500 text-transparent bg-clip-text mt-32 mb-12">ABOUT</p>
@@ -73,54 +73,56 @@ export default function Home() {
                 </div>
               </div>
               
-              <div className="flex flex-col p-10">
-                <p class="text-4xl font-bold inline-block bg-gradient-to-r from-red-500 via-yellow-500 to-green-500 text-transparent bg-clip-text mb-12">SUN, APRIL 21</p>
-                <div className="flex flex-col mb-12">
-                  <p class="text-2xl font-bold  inline-block bg-gradient-to-r from-gray-400 to-pink-200 text-transparent bg-clip-text mb-1">Collaborative Zine Making</p>
-                  <p class="text-xl font-bold text-white mb-1">12:00 - 1:00</p>
-                  <p class="text-xl font-bold text-white mb-1">Paloma B</p>
-                  <p class="text-xl text-white mb-2">MATERIALS NEEDED: Markers, Magazines, Scissors & Glue</p>
+              <div className="flex flex-row">
+                <div className="flex flex-col p-10">
+                  <p class="text-4xl font-bold inline-block bg-gradient-to-r from-red-500 via-yellow-500 to-green-500 text-transparent bg-clip-text mb-12">SUN, APRIL 21</p>
+                  <div className="flex flex-col mb-12">
+                    <p class="text-2xl font-bold  inline-block bg-gradient-to-r from-gray-400 to-pink-200 text-transparent bg-clip-text mb-1">Collaborative Zine Making</p>
+                    <p class="text-xl font-bold text-white mb-1">12:00 - 1:00</p>
+                    <p class="text-xl font-bold text-white mb-1">Paloma B</p>
+                    <p class="text-xl text-white mb-2">MATERIALS NEEDED: Markers, Magazines, Scissors & Glue</p>
+                  </div>
+                  <div className="flex flex-col mb-12">
+                    <p class="text-2xl font-bold  inline-block bg-gradient-to-r from-gray-400 to-pink-200 text-transparent bg-clip-text mb-1">Intro to 3D Modeling: Maya!</p>
+                    <p class="text-xl font-bold text-white mb-1">1:00 - 2:00</p>
+                    <p class="text-xl font-bold text-white mb-1">Linda Z</p>
+                    <p class="text-xl text-white mb-2">MATERIALS NEEDED: Maya, Adobe Photoshop</p>
+                  </div>
+                  <div className="flex flex-col mb-12">
+                    <p class="text-2xl font-bold  inline-block bg-gradient-to-r from-gray-400 to-pink-200 text-transparent bg-clip-text mb-1">Unlocking Your Artistic Potential</p>
+                    <p class="text-xl font-bold text-white mb-1">2:00 - 3:00</p>
+                    <p class="text-xl font-bold text-white mb-1">Arahan L & Kaitlyn E-S</p>
+                    <p class="text-xl text-white mb-2">MATERIALS NEEDED: Laptop</p>
+                  </div>
+                  <div className="flex flex-col mb-12">
+                    <p class="text-2xl font-bold  inline-block bg-gradient-to-r from-gray-400 to-pink-200 text-transparent bg-clip-text mb-1">Intro to Adobe After Effects</p>
+                    <p class="text-xl font-bold text-white mb-1">3:00 - 40:00</p>
+                    <p class="text-xl font-bold text-white mb-1">Linda L & Megan A</p>
+                    <p class="text-xl text-white mb-2">MATERIALS NEEDED: Laptop & Adobe After Effects</p>
+                  </div>
+                  <div className="flex flex-col mb-12">
+                    <p class="text-2xl font-bold  inline-block bg-gradient-to-r from-gray-400 to-pink-200 text-transparent bg-clip-text mb-1">The Photography Storytelling Workshop</p>
+                    <p class="text-xl font-bold text-white mb-1">4:00 - 5:00</p>
+                    <p class="text-xl font-bold text-white mb-1">Elena H & Kyle GT</p>
+                    <p class="text-xl text-white mb-2">MATERIALS NEEDED: N/A</p>
+                  </div>
+                  <div className="flex flex-col mb-12">
+                    <p class="text-2xl font-bold  inline-block bg-gradient-to-r from-gray-400 to-pink-200 text-transparent bg-clip-text mb-1">Fix it in Post: Basics of Premiere Pro</p>
+                    <p class="text-xl font-bold text-white mb-1">5:00 - 6:00</p>
+                    <p class="text-xl font-bold text-white mb-1">Daniel L & Gerry R</p>
+                    <p class="text-xl text-white mb-2">MATERIALS NEEDED: Adobe Premiere Pro</p>
+                  </div>
                 </div>
-                <div className="flex flex-col mb-12">
-                  <p class="text-2xl font-bold  inline-block bg-gradient-to-r from-gray-400 to-pink-200 text-transparent bg-clip-text mb-1">Intro to 3D Modeling: Maya!</p>
-                  <p class="text-xl font-bold text-white mb-1">1:00 - 2:00</p>
-                  <p class="text-xl font-bold text-white mb-1">Linda Z</p>
-                  <p class="text-xl text-white mb-2">MATERIALS NEEDED: Maya, Adobe Photoshop</p>
+                <div className="flex flex-col space-y-64">
+                  <img src="web.png" width={250} height={250}></img>
+                  <img src="globe_chain.png" width={250} height={250}></img>
                 </div>
-                <div className="flex flex-col mb-12">
-                  <p class="text-2xl font-bold  inline-block bg-gradient-to-r from-gray-400 to-pink-200 text-transparent bg-clip-text mb-1">Unlocking Your Artistic Potential</p>
-                  <p class="text-xl font-bold text-white mb-1">2:00 - 3:00</p>
-                  <p class="text-xl font-bold text-white mb-1">Arahan L & Kaitlyn E-S</p>
-                  <p class="text-xl text-white mb-2">MATERIALS NEEDED: Laptop</p>
-                </div>
-                <div className="flex flex-col mb-12">
-                  <p class="text-2xl font-bold  inline-block bg-gradient-to-r from-gray-400 to-pink-200 text-transparent bg-clip-text mb-1">Intro to Adobe After Effects</p>
-                  <p class="text-xl font-bold text-white mb-1">3:00 - 40:00</p>
-                  <p class="text-xl font-bold text-white mb-1">Linda L & Megan A</p>
-                  <p class="text-xl text-white mb-2">MATERIALS NEEDED: Laptop & Adobe After Effects</p>
-                </div>
-                <div className="flex flex-col mb-12">
-                  <p class="text-2xl font-bold  inline-block bg-gradient-to-r from-gray-400 to-pink-200 text-transparent bg-clip-text mb-1">The Photography Storytelling Workshop</p>
-                  <p class="text-xl font-bold text-white mb-1">4:00 - 5:00</p>
-                  <p class="text-xl font-bold text-white mb-1">Elena H & Kyle GT</p>
-                  <p class="text-xl text-white mb-2">MATERIALS NEEDED: N/A</p>
-                </div>
-                <div className="flex flex-col mb-12">
-                  <p class="text-2xl font-bold  inline-block bg-gradient-to-r from-gray-400 to-pink-200 text-transparent bg-clip-text mb-1">Fix it in Post: Basics of Premiere Pro</p>
-                  <p class="text-xl font-bold text-white mb-1">5:00 - 6:00</p>
-                  <p class="text-xl font-bold text-white mb-1">Daniel L & Gerry R</p>
-                  <p class="text-xl text-white mb-2">MATERIALS NEEDED: Adobe Premiere Pro</p>
-                </div>
-              </div>
-              <div className="flex flex-col space-y-64">
-                <img src="web.png" width={250} height={250}></img>
-                <img src="globe_chain.png" width={250} height={250}></img>
               </div>
             </div>
           </div>
-          <div className="flex flex-row">
+          <div className="flex flex-row space-x-96">
             <Image src="/car.png" width={500} height={500}></Image>
-            <Image src="/hazard_texture.png" width={300} height={100} alt="hazard"></Image>
+            <Image src="/hazard_texture.png" width={500} height={100} alt="hazard"></Image>
           </div>
         </div>
       </div>
